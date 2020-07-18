756c9ac9067e0d7f6e52c3f9980f878be2114acc
Yuvaraj Jayavel <jyuvaraj03@gmail.com>
2020-07-18 12:52:42 +0530

    Add Report model

--- api/models.py
+++ api/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from .managers import CustomUserManager
-from django.dispatch import receiver
+from django.db.models.functions import Now
+from django.contrib.postgres.fields import JSONField
 
 class CustomUser(AbstractUser):
     is_authority = models.BooleanField(default=False)
@@ -13,15 +14,26 @@ class CustomUser(AbstractUser):
 
 class Authority(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
-    district = models.CharField(max_length=200)
+    district = models.CharField(max_length=200, blank=False)
 
     def __str__(self):
         return '{} - {}'.format(self.user.username, self.district)
 
-class Reporter(models.Model):
+class School(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
-    school_name = models.CharField(max_length=250)
+    school_name = models.CharField(max_length=250, blank=False)
     authority = models.ForeignKey(Authority, on_delete=models.CASCADE)
 
     def __str__(self):
         return '{} - {}'.format(self.user.username, self.school_name)
+
+class Report(models.Model):
+    school = models.ForeignKey(School, on_delete=models.CASCADE, unique_for_date="reported_for_date")
+
+    reported_student_count = models.PositiveIntegerField(blank=False)
+    reported_menu = JSONField()
+    reported_for_date = models.DateField('date reported for', blank=False)
+    reported_on_datetime = models.DateTimeField('date and time reported on', default=Now())
+
+    estimated_student_count = models.PositiveIntegerField()
+    estimated_menu = JSONField()
