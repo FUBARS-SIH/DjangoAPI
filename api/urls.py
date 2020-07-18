a760af84c33b8bb2638144310629aa5dec291d30
vizzzzz <vishal21999@gmail.com>
2020-07-18 17:51:51 +0530

    Report restApi added

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path('getreports', views.get_reports),
   path('addreport', views.add_report),
+  path('updatebook/<int:book_id>', views.update_book),
 ]
--- api/views.py
+++ api/views.py
@@ -22,15 +22,32 @@ def add_report(request):
     payload = json.loads(request.body)
     try:
         school = School.objects.get(id=payload["school"])
-        book = Book.objects.create(
+        report = Report.objects.create(
             reported_student_count=payload["student_count"],
             reported_menu=payload["menu"],
             reported_for_date=payload["for_date"],
             reported_on_datetime=payload["on_datetime"],
             school=school
         )
-        serializer = ReportSerializer(book)
+        serializer = ReportSerializer(report)
         return JsonResponse({'report': serializer.data}, safe=False, status=status.HTTP_201_CREATED)
+    except ObjectDoesNotExist as e:
+        return JsonResponse({'error': str(e)}, safe=False, status=status.HTTP_404_NOT_FOUND)
+    except Exception:
+        return JsonResponse({'error': 'Something terrible went wrong'}, safe=False, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+@api_view(["PUT"])
+
+def update_report(request, report_id):
+    school = request.school.id
+    payload = json.loads(request.body)
+    try:
+        report_item = Report.objects.filter(added_by=school, id=report_id)
+        # returns 1 or 0
+        report_item.update(**payload)
+        report = Report.objects.get(id=report_id)
+        serializer = ReportSerializer(report)
+        return JsonResponse({'report': serializer.data}, safe=False, status=status.HTTP_200_OK)
     except ObjectDoesNotExist as e:
         return JsonResponse({'error': str(e)}, safe=False, status=status.HTTP_404_NOT_FOUND)
     except Exception:
