c79958fc737482fc468b4efea123cb75e300fa3c
Yuvaraj Jayavel <jyuvaraj03@gmail.com>
2020-07-22 19:20:47 +0530

    Complete authority report list test

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -1,10 +1,11 @@
 import json
+from datetime import date, timedelta
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
 from api.models import CustomUser, Authority, Report, District, School
-from api.serializers import AuthoritySerializer
+from api.serializers import AuthoritySerializer, FullReportSerializer
 
 class AuthorityTests(APITestCase):
 
@@ -24,9 +25,41 @@ class AuthorityTests(APITestCase):
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
 
     def create_authority_with_current_user(self):
-        self.authority = Authority.objects.create(user=self.user, district=self.district)
+        authority = Authority.objects.create(user=self.user, district=self.district)
         self.user.is_authority = True
         self.user.save()
+        return authority
+
+    def create_schools_reporting_to_authority(self, authority, num_schools):
+        schools = []
+        for i in range(num_schools):
+            school_user = CustomUser.objects.create_user(
+                username='schooluser{}'.format(i+1),
+                email='su{}@example.com'.format(i+1),
+                password='Tv#jlI2O*F'
+            )
+            school = School.objects.create(
+                user=school_user,
+                name='School {}'.format(i+1),
+                district=self.district,
+                authority=authority
+            )
+            schools.append(school)
+        return schools
+
+    def create_reports_with_school(self, school, num_reports):
+        reports = []
+        for i in range(num_reports):
+            report = Report.objects.create(
+                school=school,
+                reported_student_count=45,
+                reported_for_date=(date.today()-timedelta(i)),
+                reported_menu={},
+                estimated_student_count=50,
+                estimated_menu={}
+            )
+            reports.append(report)
+        return reports
 
     def test_authority_enroll_with_authentication(self):
         url = reverse('authority_enroll')
@@ -54,12 +87,12 @@ class AuthorityTests(APITestCase):
         url = reverse('authority_me_retrieve_update')
 
         self.api_authenticate()
-        self.create_authority_with_current_user()
+        authority = self.create_authority_with_current_user()
 
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        authority_serializer_data = AuthoritySerializer(instance=self.authority).data
+        authority_serializer_data = AuthoritySerializer(authority).data
         response_data = json.loads(response.content)
         self.assertEqual(response_data, authority_serializer_data)
 
@@ -69,14 +102,29 @@ class AuthorityTests(APITestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-    def test_authority_retrieve_update_with_authencation(self):
+    def test_authority_update_with_authencation(self):
         pass
 
-    def test_authority_retrieve_update_without_authencation(self):
+    def test_authority_update_without_authencation(self):
         pass
 
     def test_authority_report_list_with_authentication(self):
-        pass
+        url = reverse('authority_report_list')
+
+        self.api_authenticate()
+        authority = self.create_authority_with_current_user()
+        schools = self.create_schools_reporting_to_authority(authority, 2)
+        all_reports = []
+        for school in schools:
+            all_reports.extend(self.create_reports_with_school(school, 2))
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        full_report_serializer_data = FullReportSerializer(all_reports, many=True).data
+        response_data = json.loads(response.content)
+        self.assertEqual(response_data, full_report_serializer_data)
+
 
     def test_authority_report_list_without_authentication(self):
         pass
