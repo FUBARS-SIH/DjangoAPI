78bc1091b6a1598ba012ebce5a8d54898e2ef0b1
Yuvaraj Jayavel <jyuvaraj03@gmail.com>
2020-08-12 23:32:02 +0530

    Fix failing tests on CI

--- mdm/settings.py
+++ mdm/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -141,13 +140,3 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
-}
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'jyuvaraj03@gmail.com'
-EMAIL_HOST_PASSWORD = 's9G62hP%1'
-
-django_heroku.settings(locals())
